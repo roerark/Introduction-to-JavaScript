5ffa1fd5838c0784eb402be6e1b10f106ddaf85c
Joe Park <parkjs3@yahoo.com>
2020-12-17 17:43:42 -0800

    final project

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ Do the following:
    HINT: no function required
 */
 
-const class1 = 22;
+let class1 = 22;
 const class2 = 31;
 
 if (class2>=30){
@@ -168,27 +168,27 @@ let computer = Math.floor(Math.random() * 3);
 // rock = 0, paper = 1, scissors = 2 //
 
 function game (user, computer){
-    if (user === 0 && computer === 2){
+    if (user === "rock" && computer === "scissors"){
       return "you win!";
-    } else if (user === 0 && computer === 1){
+    } else if (user === "rock" && computer === "paper"){
       return "you lose!";
-    } else if (user === 0 && computer === 0){
-      return "it's a tie.";
-    } else if (user === 1 && computer === 0){
+    } else if (user === "rock" && computer === "rock"){
+      return "it's a tie";
+    } else if (user === "paper" && computer === "rock"){
       return "you win!";
-    } else if (user === 1 && computer === 2){
+    } else if (user === "paper" && computer === "scissors"){
       return "you lose!";
-    } else if (user === 1 && computer === 1){
-      return "it's a tie."
-    } else if (user === 2 && computer === 1){
-      return "you win!"
-    } else if (user === 2 && computer === 0){
-      return "you lose!"
-    } else if (user === 2 && computer === 2){
-      return "it's a tie."
+    } else if (user === "paper" && computer === "paper"){
+      return "it's a tie";
+    } else if (user === "scissors" && computer === "paper"){
+      return "you win!";
+    } else if (user === "scissors" && computer === "rock"){
+      return "you lose!";
+    } else if (user === "scissors" && computer === "scissors"){
+      return "it's a tie";
     }
 }
-console.log(0,1);
+console.log(game("rock", "rock"));
     
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -203,9 +203,9 @@ Using the miles function below do the following:
 */
 
 function miles(km){
-    let mi = km * 1.6;
+    let mi = km * 0.621371;
     return mi;
-  }
+}
 console.log(miles(2));
 
 
@@ -219,10 +219,9 @@ Using the feet function below do the following:
 */
 
 function feet(cm){
-    let ft = cm * 30.48;
-    return ft;
-    
-  }
+    let ft = cm / 30.48;
+    return ft;  
+}
 console.log(feet(3));
  
 
@@ -238,12 +237,13 @@ Using the annoyingSong function below do the following:
 */
 
 function annoyingSong(number){
-    while (number > 0){
-      let number2 = number - 1;
-      return `${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number2} left over bottles of soda on the wall`
-    }
+  while (number > 0){
+    let number2 = number - 1;
+      return `${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number2} bottles of soda on the wall`
+     number--;
+  }
 }
-console.log(annoyingSong(99));
+annoyingSong(99);
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -263,15 +263,15 @@ Using the grade function below do the following:
   
 function grade(score){
   if (score >=90 && score <=100){
-    return "You got an A"
+    return "you got an A"
   }else if (score >=80 && score <=89){
-    return "You got a B"
+    return "you got a B"
   }else if (score >= 70 && score <=79){
-    return "You got a C"
+    return "you got a C"
   }else if (score >= 60 && score <=69){
-    return "You got an D"
+    return "you got a D"
   }else if (score < 60){
-    return "You got an F"
+    return "you got an F"
   }
   }
   console.log(grade(78));
